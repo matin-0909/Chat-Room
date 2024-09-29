2d5d34dafc59335302b349b075f3769a50236784
matin-0909 <matinj0909@gmail.com>
2024-09-29 22:41:26 +0530

    refactor3

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const ChatRoom = () => {
     }, [userData]);
 
     const connect =()=>{
-        let Sock = new SockJS('https://localhost:8080/ws');
+        let Sock = new SockJS('wss://localhost:8080/ws');
         stompClient = over(Sock);
         stompClient.connect({},onConnected, onError);
     }
